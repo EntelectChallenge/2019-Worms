96936d3db57689357b77aea36603cc1c254f7f7f
Renette Ros <supergirl0894+git@gmail.com>
2019-02-16 15:17:47 +0200

    Pulled latest changes of 2018 from github

--- src/main/java/za/co/entelect/challenge/game/contracts/player/Player.java
+++ src/main/java/za/co/entelect/challenge/game/contracts/player/Player.java
@@ -1,6 +1,5 @@
 package za.co.entelect.challenge.game.contracts.player;
 
-import za.co.entelect.challenge.game.contracts.command.Command;
 import za.co.entelect.challenge.game.contracts.command.RawCommand;
 import za.co.entelect.challenge.game.contracts.game.GamePlayer;
 import za.co.entelect.challenge.game.contracts.map.GameMap;
@@ -10,33 +9,43 @@ import java.util.function.BiConsumer;
 /**
  * The base class for all players/test harnesses of the game to extend.
  * Processing of players happens in the following order.
- * 1. Player is registered to a game map, and PlayerRegistered is called.
- * 2. New Game is Started.
- * 3. Player Published a Command
- * 4. Player Waits for round to complete
- * 5. Round is Completed.
- * 6. Steps 3-5 until GameEnded is called
- * <p>
+ * <ol>
+ * <li>Player is registered to a game map, and PlayerRegistered is called.</li>
+ * <li>New Game is Started.</li>
+ * <li>Player Published a Command</li>
+ * <li>Player Waits for round to complete</li>
+ * <li>Round is Completed.</li>
+ * <li>Steps 3-5 until GameEnded is called</li>
+ * </ol>
+ * <br><p>
  * A player may only submit one command per round.  Any subsuquent commands will be ignored by the game runner. Exception may be thrown
  * A player may not submit commands to the game egine, any attempts to do so will be ignored by the game egnine. Exception may be thrown
- * <p>
+ *
+ * <br><p>
  * The game runner places no restrictions on players, and expects each player to submit a command each round, while the player is alive.
- * <p>
+ *
+ * <br><p>
  * If the test harness imposes any rules on the hosting player, such as a time limit, that restriction has to be enforced by the harness.
  * If the test harness hosts a player that can be instable, such as a network connection, or a malfunctioning bot, it must cater for that by
  * still submitting a do nothing command to the game runner every round until the game is ended, or the player is killed.
- * <p>
+ *
+ * <br><p>
  * The player entity registered to the player is linked directly to the game runner, and should this NOT BE MODIFIED IN ANY WAY!  Yeah I can
- * make a protected entity, but we are in control of the players/test harnesses, so do not mess with the game egine by chaning the entity.
- * <p>
+ * make a protected entity, but we are in control of the players/test harnesses, so do not mess with the game engine by changing the entity.
+ *
+ * <br><p>
  * If you want to log information from the game runner for the player specifically, please provide a logger by setting the property, the game runner
- * will then pass game logs on to you player.
+ * will then pass game logs on to your player.
  */
 public abstract class Player {
+
+    private int number;
 
     private String name;
 
     private GamePlayer gamePlayer;
+
+    private int timeoutCounts = 0;
 
     public Player(String name) {
         this.name = name;
@@ -66,11 +75,13 @@ public abstract class Player {
 
     public abstract void gameEnded(GameMap gameMap);
 
-    public abstract void playerKilled(GameMap gameMap);
+    public int getNumber() {
+        return number;
+    }
 
-    public abstract void playerCommandFailed(GameMap gameMap, String reason);
-
-    public abstract void firstRoundFailed(GameMap gameMap, String reason);
+    public void setNumber(int number) {
+        this.number = number;
+    }
 
     public String getName() {
         return name;
@@ -86,5 +97,13 @@ public abstract class Player {
 
     public void setGamePlayer(GamePlayer gamePlayer) {
         this.gamePlayer = gamePlayer;
+    }
+
+    public int getTimeoutCounts() {
+        return timeoutCounts;
+    }
+
+    public void incrementTimeoutCounts() {
+        this.timeoutCounts++;
     }
 }
