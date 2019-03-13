5e9c07dcdaf8a92604541871b6140e52f48a42fb
Renette Ros <supergirl0894+git@gmail.com>
2019-03-13 18:20:14 +0200

    Add docs to Direction companion functions

--- game-engine-core/src/main/za/co/entelect/challenge/game/engine/entities/Direction.kt
+++ game-engine-core/src/main/za/co/entelect/challenge/game/engine/entities/Direction.kt
@@ -16,8 +16,20 @@ enum class Direction(val shortened: String, x: Int, y: Int) {
     val vector = Point(x, y)
 
     companion object {
+
+        /**
+         * Resolves a Direction from the shortened version
+         * @param shortened Shortened form of a direction. Should correspond to the `shortened` field of a direction
+         *
+         * @throws NoSuchElementException If no Direction with the short form exists
+         */
         fun fromShortened(shortened: String): Direction = Direction.values().first { it.shortened == shortened.toUpperCase() }
 
+        /**
+         * Checks if a Direction with the shortened version exists
+         * @param shortened Shortened form of a direction. Should correspond to the `shortened` field of a direction
+         * @return true if s matching direction exists, false otherwise
+         */
         fun containsShortened(shortened: String): Boolean = Direction.values().any { it.shortened == shortened.toUpperCase() }
     }
 }
