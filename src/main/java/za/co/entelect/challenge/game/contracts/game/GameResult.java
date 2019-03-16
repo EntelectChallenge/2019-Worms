6829ab7529fd30375843400f976155e536bb41c4
Rohan Chhipa <rohan.chhipa@entelect.co.za>
2019-03-16 11:27:53 +0200

    Add players to game result

--- src/main/java/za/co/entelect/challenge/game/contracts/game/GameResult.java
+++ src/main/java/za/co/entelect/challenge/game/contracts/game/GameResult.java
@@ -1,13 +1,35 @@
 package za.co.entelect.challenge.game.contracts.game;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameResult {
 
     public String matchId = "";
     public boolean isComplete = false;
     public boolean isSuccessful = false;
-    public int winner = 0;
-    public int playerOnePoints = 0;
-    public int playerTwoPoints = 0;
-    public int roundsPlayed = 0;
     public boolean verificationRequired = true;
+    public String winner;
+    public long playerOnePoints = 0;
+    public long playerTwoPoints = 0;
+    public long roundsPlayed = 0;
+    public List<PlayerResult> playerResults;
+
+    public GameResult() {
+        playerResults = new ArrayList<>();
+    }
+
+    public void addPlayerResult(String playerId, long score) {
+        playerResults.add(new PlayerResult(playerId, score));
+    }
+
+    private class PlayerResult {
+        String playerId;
+        long score;
+
+        public PlayerResult(String playerId, long score) {
+            this.playerId = playerId;
+            this.score = score;
+        }
+    }
 }
