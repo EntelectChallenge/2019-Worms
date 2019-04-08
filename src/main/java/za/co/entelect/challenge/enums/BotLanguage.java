58e88ad99e3be6f4987296eef82d3b1eb0801c6d
Daan Keun <daan.keun@entelect.co.za>
2019-04-08 23:08:20 +0200

    Refactor out old languages, change c#core to dotnetcore

--- src/main/java/za/co/entelect/challenge/enums/BotLanguage.java
+++ src/main/java/za/co/entelect/challenge/enums/BotLanguage.java
@@ -7,43 +7,13 @@ public enum BotLanguage {
     @SerializedName("java")
     JAVA,
 
-    @SerializedName("c#")
-    CSHARP,
-
-    @SerializedName("c#core")
-    CSHARPCORE,
+    @SerializedName("dotnetcore")
+    DOTNETCORE,
 
     @SerializedName("javascript")
     JAVASCRIPT,
 
-    @SerializedName("rust")
-    RUST,
-
-    @SerializedName("c++")
-    CPLUSPLUS,
-
-    @SerializedName("python2")
-    PYTHON2,
-
-    @SerializedName("python3")
+    @SerializedName("python")
     PYTHON3,
-
-    @SerializedName("kotlin")
-    KOTLIN,
-    
-    @SerializedName("php")
-    PHP,
-
-    @SerializedName("haskell")
-    HASKELL,
-
-    @SerializedName("lisp")
-    LISP,
-
-    @SerializedName("golang")
-    GOLANG,
-
-    @SerializedName("scala")
-    SCALA,
 
 }
