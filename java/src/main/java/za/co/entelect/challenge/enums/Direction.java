0b01cd75ab8634eb660c671ac4fce5da53b70438
Rohan Chhipa <rohan.chhipa@entelect.co.za>
2019-04-13 15:49:12 +0200

    Update java bot to check for blocks

--- java/src/main/java/za/co/entelect/challenge/enums/Direction.java
+++ java/src/main/java/za/co/entelect/challenge/enums/Direction.java
@@ -2,12 +2,20 @@ package za.co.entelect.challenge.enums;
 
 public enum Direction {
 
-    N,
-    NE,
-    E,
-    SE,
-    S,
-    SW,
-    W,
-    NW
+    N(0, -1),
+    NE(1, -1),
+    E(1, 0),
+    SE(1, 1),
+    S(0, 1),
+    SW(-1, 1),
+    W(-1, 0),
+    NW(-1, -1);
+
+    public final int x;
+    public final int y;
+
+    Direction(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 }
