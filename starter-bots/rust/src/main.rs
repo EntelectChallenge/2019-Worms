547c5b0b0b67f4db8e9988457c341e2f7a5a7a5f
Justin Worthe <justin@worthe-it.co.za>
2019-04-17 21:26:01 +0200

    Rust: Structures for writing commands to stdout

--- starter-bots/rust/src/main.rs
+++ starter-bots/rust/src/main.rs
@@ -1,5 +1,22 @@
+use std::io::stdin;
+use std::io::prelude::*;
+
 mod json;
+mod command;
+
+use json::*;
+use command::*;
 
 fn main() {
-    println!("Hello, world!");
+    for line in stdin().lock().lines() {
+        let round_number = line.expect("Failed to read line from stdin: {}");
+        let state = read_state_from_json_file(&format!("./rounds/{}/state.json", round_number)).unwrap();
+        let command = choose_command(state);
+        println!("C;{};{}", round_number, command);
+    }
+}
+
+
+fn choose_command(state: State) -> Command {
+    Command::DoNothing
 }
