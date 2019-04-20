433afec33c10cae9bda98ed01d1d792c7f2904fb
jventer <jventer@fullfacing.com>
2019-04-20 12:34:20 +0200

    Removed commented code

--- starter-bots/scala/src/main/scala/za/co/entelect/challenge/enums/Direction.scala
+++ starter-bots/scala/src/main/scala/za/co/entelect/challenge/enums/Direction.scala
@@ -35,14 +35,3 @@ object Direction {
 
   case class DirectionObject(x: Int, y: Int)
 }
-
-
-
-//N(0, -1),
-//NE(1, -1),
-//E(1, 0),
-//SE(1, 1),
-//S(0, 1),
-//SW(-1, 1),
-//W(-1, 0),
-//NW(-1, -1);
